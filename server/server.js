2fe2ebf26ddb15140cbbf28a7c6ea51f4fcb27a0
rjhelm <rjhelm@github.io>
2022-10-11 20:32:25 -0600

    added concurrently to the project so server and frontend run from one terminal while developing

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { authMiddleware } = require('./utils/auth');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -33,6 +34,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// serve up static assets 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
