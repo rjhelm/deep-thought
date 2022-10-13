3c5908f4fbeda8a478cde677bf02acde98b0ce76
rjhelm <rjhelm@github.io>
2022-10-12 23:21:28 -0600

    created authorization for users with jwt and apollo, token will be used in headers to ensure user that is logged in sees specified content

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,9 +18,23 @@ const Signup = () => {
     });
   };
 
-  // submit form
+  // submit form 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    // user try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+      
+      Auth.login(data.addUser.token);
+
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -56,6 +75,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
---
+++ client/src/utils/auth.js
@@ -0,0 +1,48 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+    // retrieve data saved in token
+    getProfile() {
+        return decode(this.getToken());
+    }
+
+    // check user login status
+    loggedIn() {
+        // checks if there is a valid saved token
+        const token = this.getToken();
+        // use type coersion to check if token is NOT undefined and token NOT expired
+        return !!token && !this.isTokenExpired(token);
+    }
+
+    // check for token expiration
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    }
+
+    // retrieve token from localStorage
+    login(idToken) {
+        // saves user token to localStorage
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    // clear token from localStorage and force logout w/ reload
+    logout() {
+        // clear user token and profile data from localStorage
+        localStorage.removeItem('id_token');
+        // this will reload the page and reset state of app
+        window.location.assign('/');
+    }
+}
+
+export default new AuthService();
